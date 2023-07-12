b30c944d2521a24cec8355317ac09f6a4c0f9edd
Rahulstar12345 <rs7350245951@gmail.com>
2023-07-13 00:46:53 +0530

    This is commit by imageuploading ,image serving and deleting image are done

--- UserController.java
+++ UserController.java
@@ -2,17 +2,26 @@ package com.bikkadit.electronic.store.controller;
 
 import com.bikkadit.electronic.store.constant.AppConstants;
 import com.bikkadit.electronic.store.dto.ApiResponseMessage;
+import com.bikkadit.electronic.store.dto.ImageResponse;
 import com.bikkadit.electronic.store.dto.PageableResponse;
 import com.bikkadit.electronic.store.dto.UserDto;
+import com.bikkadit.electronic.store.service.FileService;
 import com.bikkadit.electronic.store.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -25,6 +34,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private FileService fileService;
+
+    @Value("${user.profile.image.path}")
+    private String imageUploadPath;
+
     // create
 
     /**
@@ -35,7 +50,7 @@ public class UserController {
      */
     @PostMapping
     public ResponseEntity<UserDto>  createUser(@Valid @RequestBody UserDto userDto) {
-        logger.info("Before Initializing createUser Method of Service:{}"+userDto);
+        logger.info("Before Initializing createUser Method of Controller:{}"+userDto);
         UserDto user = userService.createUser(userDto);
         logger.info("After Execution Creating User Method in Database  :{}"+userDto);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
@@ -53,7 +68,7 @@ public class UserController {
      */
     @PutMapping("/{userId}")
     public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,@PathVariable String userId) {
-        logger.info("Before Initializing updateUser Method Of Service for Id:{}"+userDto);
+        logger.info("Before Initializing updateUser Method Of Controller for Id:{}"+userDto);
         UserDto userDto1 = userService.updateUser(userDto, userId);
         logger.info("After Execution Updating User Method in Database:{}"+userDto);
         return new ResponseEntity<>(userDto1, HttpStatus.OK);
@@ -69,7 +84,7 @@ public class UserController {
      */
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponseMessage> deleteUser(@PathVariable String userId) {
-        logger.info("Before Initializing deleteUser Method Of Service for Id :{}"+userId);
+        logger.info("Before Initializing deleteUser Method Of Controller for Id :{}"+userId);
         userService.deleteUser(userId);
         ApiResponseMessage message = ApiResponseMessage.builder().message(AppConstants.USER_DELETED).success(true).status(HttpStatus.OK).build();
         logger.info("After Execution deleting User Method in Database :{}"+userId);
@@ -90,7 +105,7 @@ public class UserController {
             @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
             @RequestParam(value = "sortBy",defaultValue = "name",required = false) String sortBy,
             @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir) {
-        logger.info("Initializing getAllUser Method of Service:{}");
+        logger.info("Initializing getAllUser Method of Controller:{}");
         return new ResponseEntity<>(userService.getAllUser(pageNumber,pageSize,sortBy,sortDir), HttpStatus.OK);
 
     }
@@ -106,7 +121,7 @@ public class UserController {
      */
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUserById(@PathVariable String userId) {
-        logger.info("Initializing getUserById Method of Service for Id:{}"+userId);
+        logger.info("Initializing getUserById Method of Controller for Id:{}"+userId);
         return  new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
     }
 
@@ -122,7 +137,7 @@ public class UserController {
      */
     @GetMapping("/email/{email}")
     public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
-        logger.info("Initializing getUserByEmail Method Of Service for Email:{}"+email);
+        logger.info("Initializing getUserByEmail Method Of Controller for Email:{}"+email);
         return new ResponseEntity<>(userService.getUserByEmail(email),HttpStatus.OK);
     }
 
@@ -137,9 +152,37 @@ public class UserController {
      */
     @GetMapping("/search/{keywords}")
     public ResponseEntity<List<UserDto>> searchUser(@PathVariable String keywords) {
-        logger.info("Initializing searchUser Method Of Service for Keyword:{}"+keywords);
+        logger.info("Initializing searchUser Method Of Controller for Keyword:{}"+keywords);
         return new ResponseEntity<>(userService.searchUser(keywords), HttpStatus.OK);
     }
 
+    // upload user image
+
+    @PostMapping("/image/{userId}")
+    public ResponseEntity<ImageResponse> uploadUserImage(@RequestParam("userImage")MultipartFile image,@PathVariable String userId) throws IOException {
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+
+        UserDto user = userService.getUserById(userId);
+        user.setImageName(imageName);
+
+        UserDto userDto = userService.updateUser(user, userId);
+
+        ImageResponse imageResponse = ImageResponse.builder().imageName(imageName).message(AppConstants.IMAGE_UPLOADED).success(true).status(HttpStatus.CREATED).build();
+        return  new ResponseEntity<>(imageResponse,HttpStatus.CREATED);
+
+    }
+
+    //serve user image
+
+    @GetMapping("/image/{userId}")
+    public void serveUserImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+
+        UserDto user = userService.getUserById(userId);
+        logger.info("User image name : {} ",user.getImageName());
+        InputStream resource = fileService.getResource(imageUploadPath, user.getImageName());
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
+
+    }
 
    }
