1f4499bacc21e3a39920efbd0297114ecd52d421
Rahulstar12345 <rs7350245951@gmail.com>
2023-07-19 14:36:22 +0530

    This is commited by rahul in CategoryServiceImpl  done to add logger

--- src/main/java/com/bikkadit/electronic/store/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/bikkadit/electronic/store/service/impl/CategoryServiceImpl.java
@@ -9,6 +9,8 @@ import com.bikkadit.electronic.store.helper.Helper;
 import com.bikkadit.electronic.store.repository.CategoryRepository;
 import com.bikkadit.electronic.store.service.CategoryService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +24,8 @@ import java.util.stream.Collectors;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    private static Logger logger= LoggerFactory.getLogger(CategoryServiceImpl.class);
+
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -30,20 +34,21 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto create(CategoryDto categoryDto) {
-
+            logger.info("Initiating the Service call for the save category data {} :",categoryDto);
 
         // creating categoryId :randomly
-
         String categoryId = UUID.randomUUID().toString();
         categoryDto.setCategoryId(categoryId);
 
         Category category = mapper.map(categoryDto, Category.class);
         Category savedCategory = categoryRepository.save(category);
+        logger.info("Complete the Service call for the save category data {} :",categoryDto);
         return mapper.map(savedCategory,CategoryDto.class);
     }
 
     @Override
     public CategoryDto update(CategoryDto categoryDto, String categoryId) {
+        logger.info("Initiating the Service call for the update category data with categoryId {} :",categoryId);
         // get category of given id
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
         // update category details
@@ -52,34 +57,37 @@ public class CategoryServiceImpl implements CategoryService {
         category.setDescription(categoryDto.getDescription());
         category.setCoverImage(categoryDto.getCoverImage());
         Category updatedCategory = categoryRepository.save(category);
-
+        logger.info("Complete the Service call for the update category data with categoryId {} :",categoryId);
         return mapper.map(updatedCategory,CategoryDto.class);
     }
 
     @Override
     public void delete(String categoryId) {
-
+        logger.info("Initiating the Service call for the delete category data with categoryId {}:",categoryId);
         // get category of given id
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
+        logger.info("Complete the Service call for the delete category data with categoryId {} :",categoryId);
         categoryRepository.delete(category);
 
     }
 
     @Override
     public PageableResponse<CategoryDto> getAll(int pageNumber,int pageSize,String sortBy,String sortDir) {
+        logger.info("Initiating the Service call for the get all category data ");
         Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
 
         PageRequest pageable = PageRequest.of(pageNumber, pageSize,sort);
         Page<Category> page = categoryRepository.findAll(pageable);
         PageableResponse<CategoryDto> pageableResponse = Helper.getPageableResponse(page, CategoryDto.class);
-
+        logger.info("Complete the Service call for the get all category data ");
         return pageableResponse;
     }
 
     @Override
     public CategoryDto getCategoryId(String categoryId) {
+        logger.info("Initiating the Service call for the get category id data with categoryId {} :",categoryId);
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
-
+        logger.info("Complete the Service call for the get category id data with categoryId {} :",categoryId);
         return mapper.map(category,CategoryDto.class);
     }
 
