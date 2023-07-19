6b8a45fddc21d4821e47431468efd4fd5a3af51f
Rahulstar12345 <rs7350245951@gmail.com>
2023-07-19 15:20:54 +0530

    This is commited by rahul in CategoryController  done the document comment and modify logger

--- CategoryController.java
+++ CategoryController.java
@@ -41,38 +41,68 @@ public class CategoryController {
     private String imageUploadPath;
 
     // create
+
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote create category
+     * @param categoryDto
+     * @return
+     */
     @PostMapping
     public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
         // create service to save object
-        logger.info("Entering the request for the save category data {} :",categoryDto);
+        logger.info("Entering the request for the save category data : {}",categoryDto);
         CategoryDto categoryDto1 = categoryService.create(categoryDto);
-        logger.info("Complete the request for the save category data {} :",categoryDto);
+        logger.info("Complete the request for the save category data : {}",categoryDto);
         return new ResponseEntity<>(categoryDto1, HttpStatus.CREATED);
     }
 
     // update
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote update category
+     * @param categoryDto
+     * @param categoryId
+     * @return
+     */
     @PutMapping("/{categoryId}")
     public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto,@PathVariable String categoryId) {
-        logger.info("Entering the request for update the category data with categoryId {}:",categoryId);
+        logger.info("Entering the request for update the category data with categoryId : {}",categoryId);
         CategoryDto updatedCategory = categoryService.update(categoryDto, categoryId);
-        logger.info("Complete the request for update the category data with categoryId {} :",categoryId);
+        logger.info("Complete the request for update the category data with categoryId : {}",categoryId);
         return new ResponseEntity<>(updatedCategory,HttpStatus.OK);
     }
 
         // delete
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote delete category
+     * @param categoryId
+     * @return
+     */
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<ApiResponseMessage> deleteCategory(@PathVariable String categoryId) {
-        logger.info("Entering the request for delete the category data with categoryId {} :",categoryId);
+        logger.info("Entering the request for delete the category data with categoryId : {}",categoryId);
         categoryService.delete(categoryId);
         ApiResponseMessage responseMessage = ApiResponseMessage.builder().message(AppConstants.CATEGORY_DELETED).success(true).status(HttpStatus.OK).build();
-        logger.info("Complete the request for delete the category data with categoryId {} :",categoryId);
+        logger.info("Complete the request for delete the category data with categoryId : {}",categoryId);
         return  new ResponseEntity<>(responseMessage,HttpStatus.OK);
 
     }
 
         // get all
+
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote get all categories
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     */
     @GetMapping
     public ResponseEntity<PageableResponse<CategoryDto>>  getAll(
             @RequestParam(value = "pageNumber",defaultValue = "0",required = false) int pageNumber,
@@ -89,11 +119,18 @@ public class CategoryController {
     }
 
    // get single category detail
+
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote get single category id
+     * @param categoryId
+     * @return
+     */
     @GetMapping("/{categoryId}")
     public ResponseEntity<CategoryDto>  getCategoryId(@PathVariable String categoryId) {
-        logger.info("Entering the request for get category data with categoryId {} : ",categoryId);
+        logger.info("Entering the request for get category data with categoryId : {} ",categoryId);
         CategoryDto categoryDto = categoryService.getCategoryId(categoryId);
-        logger.info("Complete the request for get category data with categoryId {} : ",categoryId);
+        logger.info("Complete the request for get category data with categoryId : {} ",categoryId);
         return ResponseEntity.ok(categoryDto);
 
     }
@@ -102,56 +139,96 @@ public class CategoryController {
 
     // cover category image
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote upload category image
+     * @param image
+     * @param categoryId
+     * @return
+     * @throws IOException
+     */
     @PostMapping("/image/{categoryId}")
     public ResponseEntity<ImageResponse> coverCategoryImage(@RequestParam("categoryImage")MultipartFile image,@PathVariable String categoryId) throws IOException {
-        logger.info("Entering the request for upload category Image with categoryId {} :",categoryId);
+        logger.info("Entering the request for upload category Image with categoryId : {}",categoryId);
         String imageName = fileService.uploadFile(image, imageUploadPath);
         CategoryDto category = categoryService.getCategoryId(categoryId);
         category.setCoverImage(imageName);
         CategoryDto categoryDto = categoryService.update(category, categoryId);
         ImageResponse imageResponse = ImageResponse.builder().imageName(imageName).message(AppConstants.IMAGE_UPLOADED).success(true).status(HttpStatus.CREATED).build();
-        logger.info("Complete the request for upload category Image with categoryId {} :",categoryId);
+        logger.info("Complete the request for upload category Image with categoryId : {}",categoryId);
         return  new ResponseEntity<>(imageResponse,HttpStatus.CREATED);
     }
 
     // serve category image
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote serve category image
+     * @param categoryId
+     * @param response
+     * @throws IOException
+     */
     @GetMapping("/image/{categoryId}")
     public void serveCategoryImage(@PathVariable String categoryId, HttpServletResponse response) throws IOException {
-        logger.info("Entering the request for Serve category Image with categoryId {} :",categoryId);
+        logger.info("Entering the request for Serve category Image with categoryId : {}",categoryId);
         CategoryDto category = categoryService.getCategoryId(categoryId);
         logger.info("Category image name : {} ",category.getCoverImage());
         InputStream resource = fileService.getResource(imageUploadPath, category.getCoverImage());
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         StreamUtils.copy(resource, response.getOutputStream());
-        logger.info("Complete the request for Serve category Image with categoryId {} : ",categoryId);
+        logger.info("Complete the request for Serve category Image with categoryId : {} ",categoryId);
 
     }
 
     // create product with category
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote create product with category
+     * @param categoryId
+     * @param productDto
+     * @return
+     */
     @PostMapping("/{categoryId}/products")
     public ResponseEntity<ProductDto> createProductWithCategory(
             @PathVariable String categoryId,
             @RequestBody ProductDto productDto
     ){
-        logger.info("Entering the request for create product category with categoryId {} :",categoryId);
+        logger.info("Entering the request for create product category with categoryId : {}",categoryId);
         ProductDto productWithCategory = productService.createWithCategory(productDto, categoryId);
-        logger.info("Complete the request for create product category with categoryId {} :",categoryId);
+        logger.info("Complete the request for create product category with categoryId : {}",categoryId);
         return new ResponseEntity<>(productWithCategory,HttpStatus.CREATED);
     }
 
     // update category with product
 
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote upload category with product
+     * @param productId
+     * @param categoryId
+     * @return
+     */
     @PutMapping("/{categoryId}/products/{productId}")
     public ResponseEntity<ProductDto> updateCategoryoOfProduct(@PathVariable String productId,@PathVariable String categoryId) {
-        logger.info("Entering the request for update category product with categoryId {} :",categoryId);
+        logger.info("Entering the request for update category product with categoryId : {}",categoryId);
         ProductDto productDto = productService.updateCategory(productId, categoryId);
-        logger.info("Complete the request for update category product with categoryId {} :",categoryId);
+        logger.info("Complete the request for update category product with categoryId : {}",categoryId);
         return new ResponseEntity<>(productDto,HttpStatus.OK);
     }
 
 // get All By Category
+
+    /**
+     * @author Rahul_Sonawane
+     * @apiNote get all by category
+     * @param categoryId
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     */
     @GetMapping("/{categoryId}/products")
     public ResponseEntity<PageableResponse<ProductDto>> getAllByCategory
     (@PathVariable String categoryId,
@@ -159,9 +236,9 @@ public class CategoryController {
     @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
     @RequestParam(value = "sortBy",defaultValue = "title",required = false) String sortBy,
     @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir) {
-        logger.info("Entering the request for get all by category with categoryId {} :",categoryId);
+        logger.info("Entering the request for get all by category with categoryId : {}",categoryId);
         PageableResponse<ProductDto> response = productService.getAllByCategory(categoryId,pageNumber,pageSize,sortBy,sortDir);
-        logger.info("Complete the request for get all by category with categoryId {} :",categoryId);
+        logger.info("Complete the request for get all by category with categoryId : {}",categoryId);
         return new ResponseEntity<>(response,HttpStatus.OK);
 
 }
