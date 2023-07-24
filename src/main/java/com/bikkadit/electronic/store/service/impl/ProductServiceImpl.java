0f2b7d2eb7532fa077afdee74de57719d2f5a8a3
Rahulstar12345 <rs7350245951@gmail.com>
2023-07-24 18:58:14 +0530

    This is commited by rahul in Product module done

--- src/main/java/com/bikkadit/electronic/store/service/impl/ProductServiceImpl.java
+++ src/main/java/com/bikkadit/electronic/store/service/impl/ProductServiceImpl.java
@@ -51,15 +51,12 @@ public class ProductServiceImpl implements ProductService {
     public ProductDto create(ProductDto productDto) {
         logger.info("Initiating the Service call for the save product data ");
         Product product = mapper.map(productDto, Product.class);
-
         // product id
         String productId = UUID.randomUUID().toString();
         product.setProductId(productId);
-
         //added date
         product.setAddedDate(new Date());
         Product saveProduct = productRepository.save(product);
-
         logger.info("Complete the Service call for the save product data ");
         return mapper.map(saveProduct,ProductDto.class);
     }
@@ -79,7 +76,6 @@ public class ProductServiceImpl implements ProductService {
         product.setProductImageName(productDto.getProductImageName());
         // save the entity
         Product updatedProduct = productRepository.save(product);
-
         logger.info("Complete the Service call for the update product data with productId : {}",productId);
         return mapper.map(updatedProduct,ProductDto.class);
     }
@@ -88,7 +84,6 @@ public class ProductServiceImpl implements ProductService {
     public void delete(String productId) {
         logger.info("Initiating the Service call for the delete product data with productId : {}",productId);
         Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND));
-
         // delete user profile image
         // images/user/abc.png
         String fullPath = imageUploadPath + product.getProductImageName();
@@ -101,17 +96,14 @@ public class ProductServiceImpl implements ProductService {
         }catch (IOException e){
             e.printStackTrace();
         }
-
         productRepository.delete(product);
         logger.info("Complete the Service call for the delete product data with productId : {}",productId);
-
     }
 
     @Override
     public ProductDto getSingleId(String productId) {
         logger.info("Initiating the Service call for the get single product data with productId : {}",productId);
         Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND));
-
         logger.info("Complete the Service call for the get single product data with productId : {}",productId);
         return mapper.map(product,ProductDto.class);
     }
@@ -119,11 +111,9 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public PageableResponse<ProductDto> getAll(int pageNumber, int pageSize, String sortBy, String sortDir) {
         logger.info("Initiating the Service call for the get all product data ");
-
         Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
         PageRequest pageable = PageRequest.of(pageNumber, pageSize,sort);
         Page<Product> page = productRepository.findAll(pageable);
-
         logger.info("Complete the Service call for the get all product data ");
         return Helper.getPageableResponse(page,ProductDto.class);
     }
@@ -131,42 +121,35 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public PageableResponse<ProductDto> getAllLive(int pageNumber,int pageSize,String sortBy,String sortDir) {
         logger.info("Initiating the Service call for the get all live product data ");
-
         Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
         PageRequest pageable = PageRequest.of(pageNumber, pageSize,sort);
         Page<Product> page = productRepository.findByLiveTrue(pageable);
-
         logger.info("Complete the Service call for the get all live product data ");
-        return Helper.getPageableResponse(page,ProductDto.class);    }
+        return Helper.getPageableResponse(page,ProductDto.class);
+    }
 
     @Override
     public PageableResponse<ProductDto> searchByTitle(String subTitle,int pageNumber,int pageSize,String sortBy,String sortDir) {
         logger.info("Initiating the Service call for the search product by title with subTitle : {} ",subTitle);
-
         Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
         PageRequest pageable = PageRequest.of(pageNumber, pageSize,sort);
         Page<Product> page = productRepository.findByTitleContaining(subTitle,pageable);
-
         logger.info("Complete the Service call for the search product by title with subTitle : {} ",subTitle);
         return Helper.getPageableResponse(page,ProductDto.class);    }
 
     @Override
     public ProductDto createWithCategory(ProductDto productDto, String categoryId) {
         logger.info("Initiating the Service call for the create withe Category in  product with categoryId : {} ",categoryId);
-
         // fetch the category from db
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
         Product product = mapper.map(productDto, Product.class);
-
        // product id
         String productId = UUID.randomUUID().toString();
         product.setProductId(productId);
-
         //added date
         product.setAddedDate(new Date());
         product.setCategory(category);
         Product saveProduct = productRepository.save(product);
-
         logger.info("Complete the Service call for the create withe Category in  product with categoryId : {} ",categoryId);
         return mapper.map(saveProduct,ProductDto.class);
     }
@@ -174,13 +157,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDto updateCategory(String productId, String categoryId) {
         logger.info("Initiating the Service call for the update Category in  product with categoryId : {} ",categoryId);
-
         // product fetch
         Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND));
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
         product.setCategory(category);
         Product savedProduct = productRepository.save(product);
-
         logger.info("Complete the Service call for the update Category in  product with categoryId : {} ",categoryId);
         return mapper.map(savedProduct,ProductDto.class);
     }
@@ -188,13 +169,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public PageableResponse<ProductDto> getAllByCategory(String categoryId, int pageNumber,int pageSize,String sortBy,String sortDir) {
         logger.info("Initiating the Service call for the get all Category in  product with categoryId : {} ",categoryId);
-
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND));
         Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
         Pageable pageable=PageRequest.of(pageNumber,pageSize,sort);
-
         Page<Product> page = productRepository.findByCategory(category,pageable);
-
         logger.info("Complete the Service call for the get all Category in product with categoryId : {} ",categoryId);
         return Helper.getPageableResponse(page,ProductDto.class);
     }
