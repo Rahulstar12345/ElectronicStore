5ac3e605fda09108ac0e58c3b45d9a259ac930b1
Rahulstar12345 <rs7350245951@gmail.com>
2023-07-31 14:47:32 +0530

    This is commited by rahul in Product Entity we have done small changes

--- src/main/java/com/bikkadit/electronic/store/entity/Product.java
+++ src/main/java/com/bikkadit/electronic/store/entity/Product.java
@@ -1,9 +1,6 @@
 package com.bikkadit.electronic.store.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -12,6 +9,7 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name="products")
 public class Product {
