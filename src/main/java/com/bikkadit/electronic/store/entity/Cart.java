7df91ae6ad0bb4e81f7692d04e132917941a62cd
Rahulstar12345 <rs7350245951@gmail.com>
2023-08-16 18:18:59 +0530

    This is commited by rahul Cart  Entity Class and  Created Variables

--- src/main/java/com/bikkadit/electronic/store/entity/Cart.java
+++ src/main/java/com/bikkadit/electronic/store/entity/Cart.java
@@ -1,7 +1,32 @@
 package com.bikkadit.electronic.store.entity;
 
+import lombok.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="cart")
 public class Cart {
 
+    @Id
+    private String cartId;
 
+    private Date createdAt;
+
+    @OneToOne
+    private User user;
+
+    // mapping cart items
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<CartItem>  items=new ArrayList<>();
 
 }
