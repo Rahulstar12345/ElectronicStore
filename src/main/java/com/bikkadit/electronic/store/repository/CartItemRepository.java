eb7ee479e1f85b574c53d452aea96115e9456000
Rahulstar12345 <rs7350245951@gmail.com>
2023-08-17 15:17:59 +0530

    This is commited by rahul in the CartRepository we change int  to String

---
+++ src/main/java/com/bikkadit/electronic/store/repository/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.bikkadit.electronic.store.repository;
+
+import com.bikkadit.electronic.store.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
+
+}
--- src/main/java/com/bikkadit/electronic/store/repository/CartRepository.java
+++ src/main/java/com/bikkadit/electronic/store/repository/CartRepository.java
@@ -1,7 +1,12 @@
 package com.bikkadit.electronic.store.repository;
 
 import com.bikkadit.electronic.store.entity.Cart;
+import com.bikkadit.electronic.store.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart,String> {
+
+   Optional<Cart> findByUser(User user);
 }
