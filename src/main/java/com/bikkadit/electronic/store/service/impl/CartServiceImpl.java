bb0a46f3a1b9c574aade2109deff50e24bbea625
Rahulstar12345 <rs7350245951@gmail.com>
2023-08-17 15:19:13 +0530

    This is commited by rahul in the CartServiceImpl done to create additemToCart Method implemmentation

--- src/main/java/com/bikkadit/electronic/store/service/impl/CartServiceImpl.java
+++ src/main/java/com/bikkadit/electronic/store/service/impl/CartServiceImpl.java
@@ -2,10 +2,25 @@ package com.bikkadit.electronic.store.service.impl;
 
 import com.bikkadit.electronic.store.dto.AddItemToCartRequest;
 import com.bikkadit.electronic.store.dto.CartDto;
+import com.bikkadit.electronic.store.entity.Cart;
+import com.bikkadit.electronic.store.entity.CartItem;
+import com.bikkadit.electronic.store.entity.Product;
+import com.bikkadit.electronic.store.entity.User;
+import com.bikkadit.electronic.store.exception.ResourceNotFoundException;
+import com.bikkadit.electronic.store.repository.CartItemRepository;
+import com.bikkadit.electronic.store.repository.CartRepository;
 import com.bikkadit.electronic.store.repository.ProductRepository;
 import com.bikkadit.electronic.store.repository.UserRepository;
 import com.bikkadit.electronic.store.service.CartService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class CartServiceImpl implements CartService {
     @Autowired
@@ -14,15 +29,69 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CartRepository cartRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
 
     @Override
     public CartDto additemToCart(String userId, AddItemToCartRequest request) {
-        return null;
+
+        int quantity = request.getQuantity();
+        String productId = request.getProductId();
+
+        // getch the product
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Product not found in database !! "));
+        // fetch the user from db
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("user not found in database !!"));
+        Cart cart = null;
+        try{
+            cart=cartRepository.findByUser(user).get();
+        }catch(NoSuchElementException e){
+            cart=new Cart();
+            cart.setCartId(UUID.randomUUID().toString());
+            cart.setCreatedAt(new Date());
+        }
+
+        // perform cart operations
+        // if cart item already present: then update
+        AtomicReference<Boolean> updated=new AtomicReference<>(false);
+        List<CartItem> items = cart.getItems();
+        List<CartItem> updatedItems = items.stream().map(item -> {
+            if (item.getProduct().getProductId().equals(productId)) {
+            // item already present in cart
+                item.setQuantity(quantity);
+                item.setTotalPrice(quantity*product.getPrice());
+                updated.set(true);
+            }
+            return item;
+        }).collect(Collectors.toList());
+
+        cart.setItems(updatedItems);
+
+        // crerate items
+        if(!updated.get()){
+            CartItem cartItem = CartItem.builder()
+                    .quantity(quantity)
+                    .totalPrice(quantity * product.getPrice())
+                    .cart(cart)
+                    .product(product)
+                    .build();
+            cart.getItems().add(cartItem);
+        }
+        cart.setUser(user);
+        Cart updatedCart = cartRepository.save(cart);
+        return mapper.map(updatedCart,CartDto.class);
     }
 
     @Override
     public void removeItemFromCart(String userId, int cartItem) {
+
+
 
     }
 
