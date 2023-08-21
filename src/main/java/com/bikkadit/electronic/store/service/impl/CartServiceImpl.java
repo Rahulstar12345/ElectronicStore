2fdfb5837c0972e08f7f53961ed96e399c3ae4b2
Rahulstar12345 <rs7350245951@gmail.com>
2023-08-21 18:17:54 +0530

    This is commited by rahul in CartServiceImpl class is completed

--- src/main/java/com/bikkadit/electronic/store/service/impl/CartServiceImpl.java
+++ src/main/java/com/bikkadit/electronic/store/service/impl/CartServiceImpl.java
@@ -15,6 +15,7 @@ import com.bikkadit.electronic.store.repository.UserRepository;
 import com.bikkadit.electronic.store.service.CartService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -23,6 +24,7 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
+@Service
 public class CartServiceImpl implements CartService {
     @Autowired
     private ProductRepository productRepository;
@@ -70,7 +72,7 @@ public class CartServiceImpl implements CartService {
             if (item.getProduct().getProductId().equals(productId)) {
             // item already present in cart
                 item.setQuantity(quantity);
-                item.setTotalPrice(quantity*product.getPrice());
+                item.setTotalPrice(quantity*product.getDiscountedPrice());
                 updated.set(true);
             }
             return item;
@@ -82,7 +84,7 @@ public class CartServiceImpl implements CartService {
         if(!updated.get()){
             CartItem cartItem = CartItem.builder()
                     .quantity(quantity)
-                    .totalPrice(quantity * product.getPrice())
+                    .totalPrice(quantity * product.getDiscountedPrice())
                     .cart(cart)
                     .product(product)
                     .build();
